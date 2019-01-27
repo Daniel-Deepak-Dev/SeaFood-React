c194f1f96f61fd7f5920b67f4249d0513806a5a7
Obuli Raj <danatcoc94@gmail.com>
2019-01-27 17:26:43 +0530

    fish component and sample fishes
    added fish component below header component. using map function.
    
    added samplefishes using web bos funcitons

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends Component {
+  loadSampleFishes=this.loadSampleFishes.bind(this)// just know how to bind the method
   state={
     fishes:{},
     order:{}
@@ -13,14 +16,20 @@ class App extends Component {
     fishes[`fish${parseInt(Date.now()/1000000000)}`]=fish
     this.setState({fishes:fishes})
   }
+  loadSampleFishes(){
+    this.setState({fishes:sampleFishes})
+  }
   render() {
     return (
       <div className="catch-of-the-day">
         <div className='menu'>
           <Header tagline='Fresh Seafood market'/>        
+          <ul>
+            {Object.keys(this.state.fishes).map((ele)=><Fish key={ele} fishDetails={this.state.fishes[ele]}/>)}
+          </ul>
         </div>
         <Order/>
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
   }
