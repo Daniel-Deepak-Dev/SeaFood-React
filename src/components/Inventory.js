a09f9dc93e1a212002d02cbc0424b311622b90a2
Obuli Raj <danatcoc94@gmail.com>
2019-01-27 16:08:30 +0530

    1. added fish form component 2. linked a addfish prop from app.js

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import AddFishForm from './AddFish';
 
-const Header = () => {
+const Header = (props) => {
     return (
-    <div className='inventory'>inventory</div>)
+    <div className='inventory'>
+    <h2>Inventory</h2>
+    <AddFishForm addFish={props.addFish}/>
+    <AddFishForm />
+    <AddFishForm />
+    </div>)
 }
 
 export default Header;
