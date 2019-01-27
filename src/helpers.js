5003da8d1f02e480ba6f7a2f581b932e7ba7b745
Obuli Raj <danatcoc94@gmail.com>
2019-01-27 17:22:47 +0530

    changed wes' dollar to indian INR format using toLocalString function

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,7 @@
 export function formatPrice(cents) {
-  return (cents / 100).toLocaleString("en-US", {
+  return (cents / 100).toLocaleString("en-IN", {
     style: "currency",
-    currency: "USD"
+    currency: "INR"
   });
 }
 
