bed63b7052c6b5dcb30b995f55a88abffd6c9694
Obuli Raj <danatcoc94@gmail.com>
2019-01-27 11:15:04 +0530

    accessing react router and nav store page to app page

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import {getFunName} from '../helpers';
 
-const StroePicker=()=>{
+const StroePicker=(props)=>{
+    var myInput=React.createRef();
+
+    function goToStore(event){
+        event.preventDefault(); // to stop form from submitting
+        props.history.push(`/store/${myInput.current.value}`)
+    }
+
     return (
-    <form className='store-selector'>
-        <h2>Please Enter A Store</h2>
-        <input type='text' required placeholder='Enter Store Name' defaultValue={getFunName()}/>
+    <form className='store-selector' onSubmit={goToStore}>
+        <h2>Please Enter A Store Name</h2>
+        <input 
+            type='text'
+            ref={myInput} 
+            required 
+            placeholder='Enter Store Name' 
+            defaultValue={getFunName()}/>
         <button type='submit'>Visit Store -></button>
     </form>)
 }
