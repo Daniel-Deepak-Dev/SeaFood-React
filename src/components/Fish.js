094a2f8c6d5897d38a182325d682136fb0d4b792
Obuli Raj <danatcoc94@gmail.com>
2019-01-27 17:25:17 +0530

    created fish component to display fish List in header component

---
+++ src/components/Fish.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {formatPrice} from '../helpers'; 
+
+const Fish=(props)=>{
+    let {fishName :name,cost,image,description:desc}=props.fishDetails;
+
+    return(
+    <li className='menu-fish'> 
+        <img src={image} alt={name}/>
+        <h3>{name} <span className='price'>{formatPrice( cost)}</span></h3>
+        <p>{desc}</p>
+        <button>Add To Cart</button>
+    </li>)
+}
+
+export default Fish
